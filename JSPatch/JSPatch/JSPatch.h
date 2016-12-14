67dc13cbfcbe76cb041b15147cc67081aa7081a5
songqinggong <songqinggong@syswin.com>
2016-12-14 11:54:35 +0800

    JSPatch代码初始化

---
+++ JSPatch/JSPatch/JSPatch.h
@@ -0,0 +1,19 @@
+//
+//  JSPatch.h
+//  JSPatch
+//
+//  Created by 宋庆功 on 2016/12/14.
+//  Copyright © 2016年 思源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSPatch.
+FOUNDATION_EXPORT double JSPatchVersionNumber;
+
+//! Project version string for JSPatch.
+FOUNDATION_EXPORT const unsigned char JSPatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSPatch/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # JSPatch
+JSPatch的简单封装framework静态库
+
+JSPatch源代码(https://github.com/bang590/JSPatch) 下载日期 2016年12月14日11:25:28
+
+clone项目之后直接运行JSPatchAgg即可得到编译好的JSPatch.framework(真机和模拟器)
+
+1、iOS8以上
+
+2、支持bitcode
